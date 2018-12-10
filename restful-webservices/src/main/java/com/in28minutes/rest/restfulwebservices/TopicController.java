c0216fdd63a475e7dd7893829866c10341851e09
nitin <nithin.poosarla@gmail.com>
2018-12-10 11:26:44 +0530

    swagger ui ddede

--- restful-webservices/src/main/java/com/in28minutes/rest/restfulwebservices/TopicController.java
+++ restful-webservices/src/main/java/com/in28minutes/rest/restfulwebservices/TopicController.java
@@ -5,17 +5,35 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @RestController
+@EnableSwagger2
+@RequestMapping("/demo")
 public class TopicController 
 {
 	@Autowired
 	private TopicService topicserv;
+	
+	@Bean	
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("com.in28minutes.rest.restfulwebservices"))              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+	}
 	
     @RequestMapping("/topics")
 	public List<Topic> getAllTopics()
